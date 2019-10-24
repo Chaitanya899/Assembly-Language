f3ba50ea5ee56c5abadc171cdfbd67e7071da8ed
Chaitanya899 <53181662+Chaitanya899@users.noreply.github.com>
2019-10-25 00:30:57 +0530

    Update Prime.asm

--- Prime.asm
+++ Prime.asm
@@ -1,13 +1,12 @@
-
 BITS 32
 
 extern printf
 extern scanf
 
 section.data:
-         input: db "Enter the number", 0 
-         fmt1: db "It is a prime number",10,0
-         fmt2: db "It is not a prime number",10,0
+         fmt: db "%d", 0 
+         out1: db "Is prime",10,0
+         out2: db "Not prime",10,0
 
 section.text:
         global main
@@ -17,30 +16,41 @@ section.text:
         mov ebp, esp
         sub esp, 0x10
 
-        lea eax, [ebp-0x4]
+        lea eax,        [ebp-0x4]
         push eax
         push input
         call scanf
-       
-	mov ebx, [ebp-0x4]
-	
-	cmp ebx,1
-	je is prime
-	
-	cmp ebx,2
-	je is not prime
+        mov ebx,        [ebp-0x4]
 
-	loop:
-		mov wdx,0
-		mov eax,ebx
-		div ecx
-		inc ecx
-		cmp edx,0
-		je is not prime
+        cmp ebx,1
+        je isprime
 
-		cpm ecx,[ebp-0x4]
-		j1 loop
+        cmp ebx,2
+        je nprime
 
-		
+        mov ecx,2
+ 
+        loop:
+             mov edx,0
+             mov eax,ebx
+             div ecx
+             inc ecx
+             cmp edx,0
+             je nprime
+   
+             cmp ecx,   [ebp-0x4]
+             jl loop
 
-		
+        isprime:
+            
+             push outp1
+             call printf
+
+       leave
+        ret
+ 
+        nprime:
+              push outp2
+             call printf
+        leave 
+        ret  
